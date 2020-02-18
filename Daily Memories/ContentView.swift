98041f9bd0d382bc033dd0656b5cf3c11774ab6a
dlewanda <dlewanda@yahoo.com>
2020-02-17 19:36:51 -0500

    Initial Commit

---
+++ Daily Memories/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Daily Memories
+//
+//  Created by David Lewanda on 2/17/20.
+//  Copyright © 2020 LewandaCode. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
